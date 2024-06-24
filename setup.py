faffdf7f7ff381bd0bb477a213fd8adf6144d1be
Moon <165008354+Moon42Dev@users.noreply.github.com>
2024-06-25 00:05:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='sorting_algorithms',
     version='0.1.0',
-    description='A package for various sorting algorithms',
-    author='Your Name',
-    author_email='your.email@example.com',
+    description='A package for various sorting algorithms,
+    author='MaemounTouzi',
+    author_email='moontouzi@example.com',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
