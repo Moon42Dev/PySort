20d573bc58519b95c2ace24015de64d2c3e038d9
Moon <165008354+Moon42Dev@users.noreply.github.com>
2024-06-28 18:24:45 +0100

    Delete test_shell_sort.py

--- test_shell_sort.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from shell_sort import shell_sort
-
-class TestShellSort(unittest.TestCase):
-    def test_shell_sort(self):
-        self.assertEqual(shell_sort([64, 34, 25, 12, 22, 11, 90]), [11, 12, 22, 25, 34, 64, 90])
-        self.assertEqual(shell_sort([5, 1, 4, 2, 8]), [1, 2, 4, 5, 8])
-        self.assertEqual(shell_sort([3, 0, 2, 5, -1, 4, 1]), [-1, 0, 1, 2, 3, 4, 5])
